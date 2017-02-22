72b004f2bd64808db93f3fe68695d4ca99386bb5
Pedro Vitor <Pedro Vitor>
2017-02-22 07:21:20 -0300

    FirstCommit

---
+++ src/main/java/br/com/pvmeira/EagleApplication.java
@@ -0,0 +1,12 @@
+package br.com.pvmeira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EagleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EagleApplication.class, args);
+	}
+}
